4ad6f6491304f7c861dd1884bf5f8119823abed4
zainanis3 <145765912+zainanis3@users.noreply.github.com>
2023-11-03 01:09:24 +0500

    test 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        cicd test 1
+        cicd test v 2.0
         </p>
         <a
           className="App-link"

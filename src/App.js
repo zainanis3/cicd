7283ea45669f91c03697177e89f05b8d455e6887
zainanis3 <145765912+zainanis3@users.noreply.github.com>
2023-11-03 01:10:49 +0500

    update 3.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        cicd test v 2.0
+        cicd test v 3.0
         </p>
         <a
           className="App-link"
